a6410fd2a5bad844e9fbac1943f57a5fc0463d09
Takeshi Kishi <takeyaqa@gmail.com>
2025-03-19 12:57:05 +0000

    chore: Update Vitest configuration to include test files in tests directory

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    include: ['tests/*.{test,spec}.?(c|m)[jt]s?(x)'],
     environment: 'jsdom',
   },
 })
