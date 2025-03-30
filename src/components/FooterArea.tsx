7f7e830ded049ac74b0fb4d872f35ab6f0754f49
Takeshi Kishi <takeyaqa@gmail.com>
2025-03-30 07:40:21 +0000

    feat: update FooterArea component layout and styling for improved presentation

--- src/components/FooterArea.tsx
+++ src/components/FooterArea.tsx
@@ -1,7 +1,7 @@
 function FooterArea() {
   return (
-    <footer className="mt-10">
-      <div className="container mx-auto px-4">
+    <footer>
+      <div className="mx-10 mb-10 px-4 text-center">
         <p className="text-gray-500">© 2025 Takeshi Kishi</p>
       </div>
     </footer>
