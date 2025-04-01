af7b5424bb944afc1ea47373fbba25cdc9bdb00c
Takeshi Kishi <takeyaqa@gmail.com>
2025-04-01 23:36:14 +0000

    fix: adjust layout and styling for responsive design across multiple components

--- src/components/ConstraintsArea.tsx
+++ src/components/ConstraintsArea.tsx
@@ -28,24 +28,22 @@ function ConstraintsArea({
     return null
   }
   return (
-    <section className="mx-10 mb-10 rounded-md border-2 bg-gray-50 p-7 shadow-md">
+    <section className="mx-2 mb-10 rounded-md border-2 bg-gray-50 p-7 shadow-md md:mx-10">
       <div className="mb-5 grid grid-cols-12 gap-5">
         <div className="col-span-6">
-          <p className="text-lg font-bold">Constraints</p>
+          <h2 className="text-lg font-bold">Constraints</h2>
         </div>
-        <div className="col-span-3">
+        <div className="col-span-6 flex items-center justify-end gap-5">
           <button
             type="button"
-            className="w-full cursor-pointer rounded bg-gray-500 px-3 py-2 text-white hover:bg-gray-600 disabled:cursor-not-allowed disabled:opacity-50"
+            className="w-25 cursor-pointer rounded bg-gray-500 px-3 py-2 text-white hover:bg-gray-600 disabled:cursor-not-allowed disabled:opacity-50 lg:w-50"
             onClick={onAddConstraint}
           >
             Add Constraint
           </button>
-        </div>
-        <div className="col-span-3">
           <button
             type="button"
-            className="w-full cursor-pointer rounded bg-gray-500 px-3 py-2 text-white hover:bg-gray-600 disabled:cursor-not-allowed disabled:opacity-50"
+            className="w-25 cursor-pointer rounded bg-gray-500 px-3 py-2 text-white hover:bg-gray-600 disabled:cursor-not-allowed disabled:opacity-50 lg:w-50"
             onClick={onRemoveConstraint}
             disabled={constraints.length <= 1}
           >
--- src/components/OptionsArea.tsx
+++ src/components/OptionsArea.tsx
@@ -8,17 +8,18 @@ function OptionsArea({
   onEnableConstraintsArea,
 }: OptionsAreaProps) {
   return (
-    <section className="mx-10 mb-5 rounded-md border-2 bg-gray-50 p-7 shadow-md">
+    <section className="mx-2 mb-5 rounded-md border-2 bg-gray-50 p-7 shadow-md md:mx-10">
+      <h2 className="mb-5 text-lg font-bold">Options</h2>
       <div>
-        <input
-          type="checkbox"
-          className="mr-1 cursor-pointer rounded"
-          id="enable-constraints-button"
-          autoComplete="off"
-          checked={enabledConstraints}
-          onChange={onEnableConstraintsArea}
-        />
         <label className="cursor-pointer" htmlFor="enable-constraints-button">
+          <input
+            type="checkbox"
+            className="mr-1 cursor-pointer rounded"
+            id="enable-constraints-button"
+            autoComplete="off"
+            checked={enabledConstraints}
+            onChange={onEnableConstraintsArea}
+          />
           Constraints
         </label>
       </div>
