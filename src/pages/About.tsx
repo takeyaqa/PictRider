ce5db2dda9f49ffbda3252a9d58414ddbc0432a0
Takeshi Kishi <takeyaqa@gmail.com>
2025-04-20 08:43:41 +0900

    chore: update About page and add license plugin for third-party dependencies (#111)
    * chore: update About page and add license plugin for third-party dependencies
    
    * chore: update link text for third-party dependencies in About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -116,48 +116,21 @@ function About() {
           </p>
           <p className="mb-4">
             PictRider is an independent project and is not affiliated with
-            Microsoft Corporation. Microsoft PICT is included in accordance with
-            its MIT license terms.
+            Microsoft Corporation.
           </p>
         </div>
 
         <div className="mb-8">
-          <h3 className="mb-3 text-xl font-semibold">Microsoft PICT License</h3>
+          <h3 className="mb-3 text-xl font-semibold">Third-Party Libraries</h3>
           <p className="mb-4">
-            PictRider includes and distributes
+            PictRider uses the following third-party libraries:{' '}
             <a
-              href="https://github.com/microsoft/pict"
+              href="/dependencies.txt"
               className="text-blue-600 hover:underline"
-              target="_blank"
-              rel="noreferrer noopener"
             >
-              Microsoft PICT (Pairwise Independent Combinatorial Testing)
+              View third-party dependencies
             </a>
-            , which is licensed under the MIT License:
           </p>
-          <pre className="overflow-auto rounded bg-gray-100 p-4 font-mono text-sm">
-            {`MIT License
-
-Copyright (c) Microsoft Corporation. All rights reserved.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.`}
-          </pre>
         </div>
 
         <div className="mb-8">
