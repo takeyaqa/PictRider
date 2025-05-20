df982243ce90dbf7a019e4804a8d855d8e45dac2
Takeshi Kishi <takeyaqa@gmail.com>
2025-05-20 19:54:49 +0900

    refactor: introduce vitest browser mode and remove react-testing-library (#216)
    * build(deps): add @vitest/browser dependencies
    
    * Remove react-testing-library
    
    * build: add playwright dependencies installation step in test job
    
    * docs: update CONTRIBUTING.md to include Playwright dependencies installation
    
    * build: remove jsdom environment from vitest configuration
    
    * chore: add pnpm build packege setting

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,10 +6,15 @@ export default defineConfig({
   test: {
     include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     exclude: ['node_modules', 'dist', 'e2e'],
-    environment: 'jsdom',
     testTimeout: 20000,
     coverage: {
       provider: 'istanbul',
+    },
+    browser: {
+      provider: 'playwright',
+      enabled: true,
+      headless: true,
+      instances: [{ browser: 'chromium' }],
     },
   },
 })
