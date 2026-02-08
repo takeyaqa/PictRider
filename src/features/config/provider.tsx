2fed9c36cfe8e9a9af72a4b1b2bd4db772d8e810
Takeshi Kishi <takeyaqa@gmail.com>
2026-02-08 12:45:10 +0900

    chore: remove unnecessary useCallback and useMemo (maybe tamporary) (#484)

--- src/features/config/provider.tsx
+++ src/features/config/provider.tsx
@@ -1,4 +1,3 @@
-import { useCallback, useMemo } from 'react'
 import { useImmerReducer } from 'use-immer'
 import ConfigContext from './context'
 import { configReducer, getInitialConfig } from './reducer'
@@ -6,46 +5,36 @@ import { configReducer, getInitialConfig } from './reducer'
 function ConfigProvider({ children }: { children: React.ReactNode }) {
   const [config, dispatch] = useImmerReducer(configReducer, getInitialConfig())
 
-  const handleChangeConfigCheckbox = useCallback(
-    (
-      type:
-        | 'enableSubModels'
-        | 'enableConstraints'
-        | 'showModelFile'
-        | 'randomizeGeneration',
-      checked: boolean,
-    ) => {
-      dispatch({
-        type,
-        payload: { checked },
-      })
-    },
-    [dispatch],
-  )
+  const handleChangeConfigCheckbox = (
+    type:
+      | 'enableSubModels'
+      | 'enableConstraints'
+      | 'showModelFile'
+      | 'randomizeGeneration',
+    checked: boolean,
+  ) => {
+    dispatch({
+      type,
+      payload: { checked },
+    })
+  }
 
-  const handleChangeConfigInput = useCallback(
-    (
-      type: 'orderOfCombinations' | 'randomizeSeed',
-      e: React.ChangeEvent<HTMLInputElement>,
-    ) => {
-      dispatch({
-        type,
-        payload: { e },
-      })
+  const handleChangeConfigInput = (
+    type: 'orderOfCombinations' | 'randomizeSeed',
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    dispatch({
+      type,
+      payload: { e },
+    })
+  }
+  const value = {
+    config,
+    handlers: {
+      handleChangeConfigCheckbox,
+      handleChangeConfigInput,
     },
-    [dispatch],
-  )
-
-  const value = useMemo(
-    () => ({
-      config,
-      handlers: {
-        handleChangeConfigCheckbox,
-        handleChangeConfigInput,
-      },
-    }),
-    [config, handleChangeConfigCheckbox, handleChangeConfigInput],
-  )
+  }
 
   return <ConfigContext value={value}>{children}</ConfigContext>
 }
